375143370481b4e3548df0475ac1a80970369fc8
Nick Salloum <nick@callmenick.com>
2015-01-20 19:14:53 +0800

    added grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default tasks.
   grunt.registerTask('default', ['concat','uglify','cssmin']);
